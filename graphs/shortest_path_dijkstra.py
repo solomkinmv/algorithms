095082497bd0c08ebfabf27c69bc4444a0b0d3b3
Maxim Solomkin <solomkinmv@gmail.com>
2015-11-07 20:15:31 +0200

    Added Dijkstra algorithm.

---
+++ graphs/shortest_path_dijkstra.py
@@ -0,0 +1,37 @@
+def dijkstra(G, start, end):
+    distances = {}
+    precusors = {}
+    V = set(G.keys())
+    for v in G:
+        distances[v] = float('inf')
+        precusors[v] = None
+
+    distances[start] = 0
+    x = {start}
+    v = start  # starting point
+    while x != V:
+        for u in G[v]:
+            if distances[u] > distances[v] + 1:
+                distances[u] = distances[v] + 1
+                precusors[u] = v
+
+        v = min(V - x, key=lambda arg: distances[arg])
+        x.add(v)
+        # if v == end:
+        #     break
+
+    return distances, precusors
+
+
+if __name__ == '__main__':
+    graph = {}
+    print 'Graph initialization'
+    graph['s'] = ['a', 'b']
+    graph['a'] = ['c', 'd', 's']
+    graph['b'] = ['d', 's']
+    graph['c'] = ['a', 'd', 'e']
+    graph['d'] = ['a', 'b', 'c', 'e']
+    graph['e'] = ['c', 'd']
+
+    print 'Distances'
+    print dijkstra(graph, 's', 'a')
