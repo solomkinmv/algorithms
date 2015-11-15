bee533e10c92a55370147577071a04bb9d16c1b6
Maxim Solomkin <solomkinmv@gmail.com>
2015-11-15 11:52:29 +0200

    Fixed bug with wrong bounds.

--- sorting-algorithms/BubbleSort.java
+++ sorting-algorithms/BubbleSort.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 class BubbleSort {
     static int[] bubbleSort(int[] array) {
         for (int i = array.length - 1; i >= 0; i--) {
-            for (int j = 1; j < i; j++) {
+            for (int j = 1; j < i + 1; j++) {
                 if (array[j - 1] > array[j]) {
                     int tmp = array[j - 1];
                     array[j - 1] = array[j];
@@ -16,6 +16,6 @@ class BubbleSort {
     }
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(bubbleSort(new int[]{4, 7, 1, 0, 2, 7, 4, 6, 9})));
+        System.out.println(Arrays.toString(bubbleSort(new int[]{4, 7, 1, 0, 2, 7, 4, 6, -1})));
     }
 }
