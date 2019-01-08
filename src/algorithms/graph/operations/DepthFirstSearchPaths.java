53f0e1ec2af069c6a33de7270c318eaf21349434
Maksym Solomkin <solomkinmv@gmail.com>
2019-01-09 00:25:07 +0200

    Refactor to use map instead of array

--- src/algorithms/graph/operations/DepthFirstSearchPaths.java
+++ src/algorithms/graph/operations/DepthFirstSearchPaths.java
@@ -3,27 +3,30 @@ package algorithms.graph.operations;
 import algorithms.graph.Graph;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class DepthFirstSearchPaths {
     private final Graph graph;
     private final int source;
-    private final boolean[] marked;
-    private final int[] pathTo;
+    private final Map<Integer, Boolean> marked = new HashMap<>();
+    private final Map<Integer, Integer> pathTo = new HashMap<>();
 
     public DepthFirstSearchPaths(Graph graph, int source) {
         this.graph = graph;
         this.source = source;
-        marked = new boolean[graph.getNumberOfVertices()];
-        pathTo = new int[graph.getNumberOfVertices()];
+
+        graph.getVertices()
+             .forEach(v -> marked.put(v, false));
 
         dfs(source);
     }
 
     public boolean hasPathTo(int destination) {
-        return marked[destination];
+        return marked.getOrDefault(destination, false);
     }
 
     public Optional<Collection<Integer>> pathTo(int destination) {
@@ -32,7 +35,7 @@ public class DepthFirstSearchPaths {
             LinkedList<Integer> result = new LinkedList<>();
             while (current != source) {
                 result.addFirst(current);
-                current = pathTo[current];
+                current = pathTo.get(current);
             }
             result.addFirst(current);
             return Optional.of(result);
@@ -68,10 +71,10 @@ public class DepthFirstSearchPaths {
     }
 
     private void recordPathToVertex(int vertex, int parentVertex) {
-        pathTo[vertex] = parentVertex;
+        pathTo.putIfAbsent(vertex, parentVertex);
     }
 
     private void markVertexAsInvestigated(int vertex) {
-        marked[vertex] = true;
+        marked.put(vertex, true);
     }
 }
